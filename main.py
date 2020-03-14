0eb72bfe04c2f34bb290c64be2465a6bc16097d2
finnegantdewitt <finn@finnegandewitt.com>
2020-03-14 10:54:21 -0700

    added score

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from pygame.locals import *
 SIZE = WIDTH, HEIGHT = 1000, 1000
 SCREENRECT = pygame.Rect(0, 0, WIDTH, HEIGHT)
 ALIEN_RELOAD = 12
+SCORE = 0
 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
@@ -60,18 +61,49 @@ class Rocket(pygame.sprite.Sprite):
     def update(self):
         self.rect.move_ip([0, -10])
 
+class Score(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.font = pygame.font.SysFont("Ariel", 21)
+        self.color = Color('White')
+        self.lastscore = -1
+        self.update()
+        self.rect = self.image.get_rect().move(10, 450)
+        self.rect.right = WIDTH-40
+        self.rect.top = 15
+
+    def update(self):
+        if(SCORE !=  self.lastscore):
+            self.lastscore = SCORE
+            msg = "Score: %d" % SCORE
+            self.image = self.font.render(msg, 0, self.color)
+
 class Alien(pygame.sprite.Sprite):
     images = []
+    flap_freq = 5 
     speed = 7
     def __init__(self):
         pygame.sprite.Sprite.__init__(self, self.containers)
         self.image = self.images[0]
+        self.img_num = 0
         self.rect = self.image.get_rect()
         self.rect.right = WIDTH
+        self.rect.top = 50
         self.facing = -1 * Alien.speed
+        self.flap_counter = 0
 
     def update(self):
         self.rect.move_ip(self.facing, 0)
+        self.flap_counter += 1
+        if(self.flap_counter == self.flap_freq):
+            if(self.img_num == 0):
+                self.image = self.images[1]
+                self.img_num = 1
+            else:
+                self.image = self.images[0]
+                self.img_num = 0
+            self.flap_counter = 0
+
         if(not SCREENRECT.contains(self.rect)):
             self.facing = -self.facing
             self.rect.top = self.rect.bottom + 1
@@ -107,9 +139,9 @@ def main():
     background.fill(BLACK)
 
     #load images
-    Ship.images = [load_image("sprites/Ship_White.png")]
-    Rocket.images = [load_image("sprites/rocket_0001.png")]
-    Alien.images = [load_image("sprites/Fly_0001.png")]
+    Ship.images = load_images("sprites/Ship_White.png")
+    Rocket.images = load_images("sprites/rocket_0001.png")
+    Alien.images = load_images("sprites/mosquito_0001.png", "sprites/mosquito_0002.png")
     Explosion.images = load_images("sprites/enemy_explosion_0001.png", "sprites/enemy_explosion_0002.png", "sprites/enemy_explosion_0003.png", "sprites/enemy_explosion_0004.png")
 
     # initialize game groups
@@ -122,9 +154,11 @@ def main():
     Rocket.containers = rockets, all
     Alien.containers = aliens, all
     Explosion.containers = all
+    Score.containers = all
 
     # create some starting values
     clock = pygame.time.Clock()
+    global SCORE
 
     # starting variables
     alienreload = ALIEN_RELOAD
@@ -132,6 +166,7 @@ def main():
     # init sprites
     ship = Ship()
     Alien()
+    all.add(Score())
 
     while 1:
         for event in pygame.event.get():
@@ -164,6 +199,7 @@ def main():
         # Detect collisions
         for alien in pygame.sprite.groupcollide(rockets, aliens, 1, 1):
             Explosion(alien)
+            SCORE += 1
 
 
         #draw the scene
